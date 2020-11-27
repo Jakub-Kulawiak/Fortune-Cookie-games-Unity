83fd3c12579e7f40e1d6533381c4c9d2582557b6
Jakub Kulawiak <Jakub.Kulawiak@georgebrown.ca>
2020-11-26 19:07:03 -0500

    Most recent, functional
    Has most features as in death boxes, jump limits, and proper animations

---
+++ Chrono Dash/Assets/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Instant Death")
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+    }
+}
